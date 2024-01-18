c071dc4f76e66888bb1be7d9999f4e1bcbfc51f7
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-01-18 17:47:37 +0530

    Update unordered_set.cpp

--- unordered_set.cpp
+++ unordered_set.cpp
@@ -41,42 +41,13 @@ int main(){
 	//to check if the unordered set is empty or not
 	s.empty();
 
-	unordered_set < int > s;
-	for (int i = 1; i <= 10; i++) {
-		s.insert(i);
-	}
-	cout << "Elements present in the unordered set: ";
+	cbegin() – it refers to the first element of the unordered set.
 
+	cend() – it refers to the theoretical element after the last element of the unordered set.
 
-	for (auto it = s.begin(); it != s.end(); it++) {
-		cout << * it << " ";
-	}
-	cout << endl;
+	bucket_size() – gives the total number of elements present in a specific bucket in an unordered set.
 
+	max_size() – the maximum elements an unordered_set can hold.
 
-	int n = 2;
-	if (s.find(2) != s.end())
-		cout << n << " is present in unordered set" << endl;
-
-
-	s.erase(s.begin());
-	cout << "Elements after deleting the first element: ";
-
-
-	for (auto it = s.begin(); it != s.end(); it++) {
-		cout << * it << " ";
-	}
-	cout << endl;
-
-	cout << "The size of the unordered set is: " << s.size() << endl;
-
-
-	if (s.empty() == false)
-		cout << "The unordered set is not empty " << endl;
-	else
-		cout << "The unordered set is empty" << endl;
-
-	
-	s.clear();
-	cout << "Size of the unordered set after clearing all the elements: " << s.size();
+	max_bucket_count() – to check the maximum number of buckets an unordered set can hold.
 }
